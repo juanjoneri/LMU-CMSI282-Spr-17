5ab5dbc7baec10debf268d42152494bf56fccdb6
Juan Neri <Juanjo.neri@gmail.com>
2017-01-24 01:32:41 -0800

    modify filereader so it will give a flag in case of bad data

--- KthSmallest/FileReader.java
+++ KthSmallest/FileReader.java
@@ -15,20 +15,33 @@ public class FileReader {
         stdIn = new java.io.BufferedReader (new java.io.InputStreamReader(System.in));
     }
 
+    /**
+     * @return an int array holding the integers found in stdIn, or an empty array if there is a problem
+     */
     public int[] readNumbers () {
         ArrayList<Integer> numbers = new ArrayList<>();
         String line = null;
         try {
-            while ((line  = stdIn.readLine()) != null) numbers.add(readNumber(line));
+            while ((line  = stdIn.readLine()) != null) {
+                int toAdd = readNumber(line);
+                if (toAdd < Integer.MAX_VALUE) numbers.add(toAdd);
+                else return new int[] {};
+            }
         } catch (IOException e) {
             System.out.println("cannot read file ");
         }
         return convertIntegers(numbers);
     }
 
+    /**
+     * @return the integer value of the string, or MAX_VALLUE in case the string is not a number
+     */
     private int readNumber (String line) {
-        //let java figure out what to do with illegal arguments
-        return Integer.parseInt(line);
+        try {
+            return Integer.parseInt(line);
+        } catch (NumberFormatException nfe) {
+            return Integer.MAX_VALUE;
+        }
     }
 
     public static int[] convertIntegers(ArrayList<Integer> integers) {
