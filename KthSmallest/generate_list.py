407e2c1db13f974d99e2c96e85b177e287372e24
Juan Neri <Juanjo.neri@gmail.com>
2017-01-23 17:26:01 -0800

    add python script for creating a list to test KthSmallest

---
+++ KthSmallest/generate_list.py
@@ -0,0 +1,18 @@
+"""
+use this script to generate a list to debug
+KthSmallest. java
+In this lists, Kth is just k
+"""
+
+import random
+data = list(range(1,100))
+random.shuffle(data)
+for i in data:
+    print(i);
+
+"""
+print your data twice and Kth will be... k/2
+random.shuffle(data)
+for i in data:
+    print(i);
+"""
